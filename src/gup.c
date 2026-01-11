86824c1498092660c690ee64ab74184f42617729
Ian Moffett <ian@lunota.org>
2026-01-10 21:56:28 -0500

    gup: Add initial arg parsing
    Signed-off-by: Ian Moffett <ian@lunota.org>

--- src/gup.c
+++ src/gup.c
@@ -1,13 +1,56 @@
 /*
- * Copyright (c) 2025, Ian Moffett.
+ * Copyright (c) 2026, Ian Moffett.
  * Provided under the BSD-3 clause.
  */
 
 #include <stdio.h>
+#include <unistd.h>
+
+#define GUP_VERSION "0.0.1"
+
+static void
+help(void)
+{
+    printf(
+        "the gup compiler - gup!\n"
+        "-----------------------------\n"
+        "[-h]   Display this help menu\n"
+        "[-v]   Display the version\n"
+    );
+}
+
+static void
+version(void)
+{
+    printf(
+        "------------------------------\n"
+        "gup compiler -- v%s\n"
+        "Copyright (c) 2026 Ian Moffett\n"
+        "------------------------------\n",
+        GUP_VERSION
+    );
+}
 
 int
-main(void)
+main(int argc, char **argv)
 {
-    printf("Hello, World!\n");
+    int opt;
+
+    if (argc < 2) {
+        printf("fatal: too few arguments!\n");
+        help();
+        return -1;
+    }
+
+    while ((opt = getopt(argc, argv, "hv")) != -1) {
+        switch (opt) {
+        case 'h':
+            help();
+            return -1;
+        case 'v':
+            version();
+            return -1;
+        }
+    }
     return 0;
 }
