2b61f9d988269851419ba362cd7f1d7b402075b9
Ian Moffett <ian@lunota.org>
2026-01-11 11:29:37 -0500

    gup: Add data types
    Signed-off-by: Ian Moffett <ian@lunota.org>

---
+++ inc/gup/types.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2026, Ian Moffett.
+ * Provided under the BSD-3 clause.
+ */
+
+#ifndef GUP_TYPE_H
+#define GUP_TYPE_H 1
+
+/*
+ * Represents valid program types besides
+ * GUP_TYPE_BAD
+ */
+typedef enum {
+    GUP_TYPE_BAD,
+    GUP_TYPE_VOID,
+    GUP_TYPE_U8,
+    GUP_TYPE_U16,
+    GUP_TYPE_U32,
+    GUP_TYPE_U64
+} gup_type_t;
+
+/*
+ * Represents the specific type of a piece of data
+ *
+ * @type: Type of data
+ */
+struct datum_type {
+    gup_type_t type;
+};
+
+#endif  /* !GUP_TYPE_H */
